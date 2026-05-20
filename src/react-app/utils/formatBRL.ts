09173d8c1a6b0c2c182d8a6e421d2c9abcd05bea
sgmultimidia <sgmultimidiasps@gmail.com>
2026-05-20 14:17:21 -0300

    Add files via upload

---
+++ src/react-app/utils/formatBRL.ts
@@ -0,0 +1,7 @@
+// Formata valor monetário no padrão brasileiro
+export function formatBRL(value: number): string {
+  return value.toLocaleString('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
